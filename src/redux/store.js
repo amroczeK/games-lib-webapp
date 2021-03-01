49af278f2862fba1edcbdfdfd0267cc8fa8d05ec
amroczeK <mroczek9990@gmail.com>
2021-03-01 13:59:26 +0800

    redux store, actions, reducer setup

---
+++ src/redux/store.js
@@ -0,0 +1,20 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { gamesReducer } from '../redux/reducers/gamesReducer';
+
+const rootReducer = combineReducers({
+  games: gamesReducer,
+});
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
